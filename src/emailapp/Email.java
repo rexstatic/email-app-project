53cdfd072e655c32b340a9ddaf6bb89215a18980
rexstatic <paw94@live.dk>
2020-07-24 15:57:17 +0200

    done

--- src/emailapp/Email.java
+++ src/emailapp/Email.java
@@ -4,11 +4,14 @@ import java.util.Scanner;
 public class Email {
     private String firstName;
     private String lastName;
+    private String password;
     private String department;
     private String company;
     private String alternateEmail;
-    private int mailBoxCapacity;
-    
+    private int mailBoxCapacity = 500;
+    private int defaultPasswordLength = 16;
+    private String companySuffix = "SAMSUNG.com";
+    private String email;
     //Contructor to receive first and last name
     public Email(String firstName, String lastName) {
         this.firstName = firstName;
@@ -18,13 +21,20 @@ public class Email {
         // Call a method asking for the department - return department
         this.department = setDepartment(); 
         System.out.println("Department: " + this.department);
+
+        // Call a mathod that returns a random password
+        this.password = randomPassword(defaultPasswordLength);
+        System.out.println("Password is: " + this.password);
+
+        // Combine email
+        email = firstName.toLowerCase() + "." + lastName.toLowerCase() + "@" + department.toLowerCase() + "." + companySuffix;
+        System.out.println(email);
     }
     // Ask for the department
     private String setDepartment() {
-        System.out.print("Department code\n1 for Sales\n2 for Development\n3 for accounting\n0 for none\nEnter code choice: ");
+        System.out.print("Department code\n1 for Sales\n2 for Development\n3 for Accounting\n0 for none\nEnter code choice: ");
         Scanner in = new Scanner(System.in);
         int depChoice = in.nextInt();
-
         /* It's done this way to only have one exit with return dep.
         With multiple exits dep wouldn't be needed and a return in switch case would
         replace dep and break; would be removed.
@@ -44,16 +54,37 @@ public class Email {
                 switchResult = "";
                 break;
         }
+        in.close();
         return switchResult;
 
 
     }
     // Generate a random password
+    private String randomPassword(int length) {
+        String passwordSet = "ABCDEFGHIKLMNOPQRSTYWXYZ0123456789!@#$%";
+        char[] password = new char[length];
+        for (int i=0; i<length; i++) {
+            int rand = (int) (Math.random() * passwordSet.length());
+            password[i] = passwordSet.charAt(rand);
+        }
+        return new String(password);
+    }
 
     // Set mailbox capacity
+    public void setMailboxCapacity(int capacity) {
+        this.mailBoxCapacity = capacity;
+    }
 
     // Set alternate email
-
+    public void setAlternateEmail(String altEmail) {
+        this.alternateEmail = altEmail;
+    }
     // Change the password
+    public void changePassword(String password) {
+        this.password = password;
+    }
 
+    public int getMailboxCapacity() {return mailBoxCapacity; }
+    public String getAlternateEmail() {return alternateEmail; }
+    public String getPassword() {return password; }
 }
--- src/emailapp/EmailApp.java
+++ src/emailapp/EmailApp.java
@@ -2,5 +2,8 @@ package emailapp;
 public class EmailApp{
     public static void main(String[] args) {
         Email em1 = new Email("Simon", "Hansen");
+
+        em1.setAlternateEmail("jvc@SAMSUNG.com");
+        System.out.println("Alternate email: " + em1.getAlternateEmail());
     }
 }
